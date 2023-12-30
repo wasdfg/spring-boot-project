12392127b14ad172ea82681ba6bfb3846562b64b
wasdfg <leejh9711@naver.com>
2023-12-30 17:16:33 +0900

    spring boot 버전 변경 및 이에 따른 코드 수정

--- src/main/java/com/wasd/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/wasd/book/springboot/config/auth/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,20 +16,26 @@ public class SecurityConfig{
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable()
+                )
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(frameOptionsConfig ->
+                                frameOptionsConfig.disable()
+                        )
+                )
+
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
                         .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
                         .anyRequest().authenticated())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/"))
-                .oauth2Login(oauth2Login -> oauth2Login
-                        .userInfoEndpoint()
-                        .userService(customOAuth2UserService));
+                .oauth2Login((oauth2Login) -> oauth2Login
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                        .userService(customOAuth2UserService)));
         return http.build();
     }
 }
