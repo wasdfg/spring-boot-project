a451de565ec5e7b7a9eb32d22b6252c99ed9879c
wasdfg <leejh9711@naver.com>
2023-12-31 16:20:35 +0900

    deprecated된 코드 수정 applicaiotn_json_utf-8에서 utf-8을 제거

--- src/test/java/com/wasd/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/wasd/book/springboot/web/PostsApiControllerTest.java
@@ -77,7 +77,7 @@ public class PostsApiControllerTest {
 
         //when
         mvc.perform(post(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(requestDto)))
                 .andExpect(status().isOk());
 
@@ -110,7 +110,7 @@ public class PostsApiControllerTest {
 
         //when
         mvc.perform(put(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(requestDto)))
                 .andExpect(status().isOk());
 
